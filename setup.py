92e4505e5cd3eacb10ad0d7434c95ca009d08f76
Hong-She Liang <starofrainnight@gmail.com>
2019-01-10 21:23:48 +0800

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ tests_requires = [
 
 setup(
     name="freeplane-helper",
-    version="0.0.1",
+    version="version='0.0.2'",
     description="A script that help convert FreePlane document to other formats",
     long_description=long_description,
     author="Hong-She Liang",
