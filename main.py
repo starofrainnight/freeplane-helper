2261158a55c6429e8cd7b75f2e09ec09d2c951b9
Hong-She Liang <starofrainnight@gmail.com>
2019-01-10 16:55:58 +0800

    Generate pdf from odt format by libreoffice

--- main.py
+++ main.py
@@ -2,13 +2,84 @@
 
 import os
 import os.path
+import sys
 import click
 import shutil
 import subprocess
 import re
 import pypandoc
+import fnmatch
+import glob
 from whichcraft import which
 from rabird.core.configparser import ConfigParser
+
+
+def search_cmd(cmd, mode=os.F_OK | os.X_OK, path=None):
+    """Given a command, mode, and a PATH string, return the path which
+    conforms to the given mode on the PATH, or None if there is no such
+    file.
+    `mode` defaults to os.F_OK | os.X_OK. `path` defaults to the result
+    of os.environ.get("PATH"), or can be overridden with a custom search
+    path.
+    Note: This function was backported from the Python 3 source code.
+    """
+    # Check that a given file can be accessed with the correct mode.
+    # Additionally check that `file` is not a directory, as on Windows
+    # directories pass the os.access check.
+
+    def _access_check(fn, mode):
+        return (
+            os.path.exists(fn) and os.access(fn, mode) and not os.path.isdir(fn)
+        )
+
+    # If we're given a path with a directory part, look it up directly
+    # rather than referring to PATH directories. This includes checking
+    # relative to the current directory, e.g. ./script
+    if os.path.dirname(cmd):
+        if _access_check(cmd, mode):
+            return cmd
+
+        return None
+
+    if path is None:
+        path = os.environ.get("PATH", os.defpath)
+    if not path:
+        return None
+
+    path = path.split(os.pathsep)
+
+    if sys.platform == "win32":
+        # The current directory takes precedence on Windows.
+        if os.curdir not in path:
+            path.insert(0, os.curdir)
+
+        # PATHEXT is necessary to check on Windows.
+        pathext = os.environ.get("PATHEXT", "").split(os.pathsep)
+        # See if the given file matches any of the expected path
+        # extensions. This will allow us to short circuit when given
+        # "python.exe". If it does match, only test that one, otherwise we
+        # have to try others.
+        if any(cmd.lower().endswith(ext.lower()) for ext in pathext):
+            files = [cmd]
+        else:
+            files = [cmd + ext for ext in pathext]
+    else:
+        # On other platforms you don't have things like PATHEXT to tell you
+        # what file suffixes are executable, so just pass on cmd as-is.
+        files = [cmd]
+
+    seen = set()
+    for dir in path:
+        normdir = os.path.normcase(dir)
+        if normdir not in seen:
+            seen.add(normdir)
+            cmd_pattern = os.path.join(dir, cmd)
+            files = glob.glob(cmd_pattern)
+            for thefile in files:
+                name = os.path.join(dir, thefile)
+                if _access_check(name, mode):
+                    return name
+    return None
 
 
 def fp_get_user_dir():
@@ -104,7 +175,6 @@ def fp_fix_markdown(md_doc):
 def main(fp_doc):
     """A script to convert Freeplane document to Markdown correctly.
     """
-
     fp_ensure_script_executable()
 
     # Copy groovy export script for Freeplane
@@ -130,9 +200,12 @@ def main(fp_doc):
     # markdown file. If you want to generate a correctly PDF file, you must
     # use pandoc to convert this markdown file to odt file, then use libreoffice
     # to fix the section numbers issue, finally generate PDF by libreoffice.
-    pypandoc.convert_file(
-        md_doc, "odt", outputfile=os.path.splitext(md_doc)[0] + ".odt"
-    )
+    odt_doc = os.path.splitext(md_doc)[0] + ".odt"
+    pypandoc.convert_file(md_doc, "odt", outputfile=odt_doc)
+
+    # Generate PDF by libreoffice (TODO: Still have section numbers issues)
+    libreoffice_cmd = search_cmd("libreoffice*")
+    subprocess.call([libreoffice_cmd, "--convert-to", "pdf", odt_doc])
 
 
 if __name__ == "__main__":
